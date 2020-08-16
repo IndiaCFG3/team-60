3c49e4efd41bb03a76d111da2037910631546a0e
dharanipriya02 <dharanipriya0002@gmail.com>
2020-08-16 13:11:29 +0530

    added models

--- abc/ngo/app1/models.py
+++ abc/ngo/app1/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class OprationManager(models.Model):
+    Op_name=models.CharField(max_length=34)
+    Op_email=models.CharField(max_length=40)
+    Op_phn=models.IntegerField(max_length=10)
+    Op_password=models.CharField(max_length=50)
+
+class Op_to_Mob(models.Model):
+    Op_name=models.CharField(max_length=34)
+    Mob_name=models.CharField(max_length=100)
